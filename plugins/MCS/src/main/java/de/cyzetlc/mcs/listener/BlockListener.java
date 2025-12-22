b38c76608e49033238219534976e9f3926b5afe8
Tom <57322153+CyZeTLC@users.noreply.github.com>
2025-12-22 19:26:30 +0100

    Added license text to all java classes

--- plugins/MCS/src/main/java/de/cyzetlc/mcs/listener/BlockListener.java
+++ plugins/MCS/src/main/java/de/cyzetlc/mcs/listener/BlockListener.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2025 Tom Coombs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.cyzetlc.mcs.listener;
 
 import de.cyzetlc.mcs.Main;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,8 +27,6 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.EnumSet;
